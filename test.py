dfbfd2e97985b610cc9660b151121a0c08e728d6
YongdongHe <heyongdonghe@qq.com>
2016-07-26 19:44:08 +0800

    修复了LENGTH树没有写的问题

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #coding=utf8
-from MyZipUtils import *
+#from MyZipUtils import *
 import struct
 print ord('t')
+a = ""
+print a[::-1]
 print len('and fifth places.The survey was commissioned by the Reading Agency, which has launched a Summer Reading Challenge for children in libraries across the country.“We know the power of picking up a book you love ')
